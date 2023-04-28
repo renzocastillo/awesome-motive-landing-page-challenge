e31e037b469e009feac0e6ed491a5ada12e5d1a5
Renzo Castillo <renzo.castillo@quaira.com>
2023-04-28 18:35:10 -0500

    css styles adjustments for mobile responsive

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ const playVideo = (event) => {
     const iframe = document.createElement("iframe");
     iframe.setAttribute("src", `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0`);
     iframe.setAttribute("allowfullscreen", "");
-    iframe.style.width = "960px";
-    iframe.style.height = "540px";
+    iframe.style.width = "100%";
+    iframe.style.height = "auto";
     
     const thumbnail = document.getElementById("video-thumbnail-click");
     thumbnail.style.display = "none";
